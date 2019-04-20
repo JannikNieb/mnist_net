e14589ef067c31c6a3177a75a609ac712383d188
Jannik Niebling <jannik.niebling@gmx.de>
2019-04-20 20:14:22 +0200

    add batch size as additional dimension to all layers

--- cnn_main.py
+++ cnn_main.py
@@ -3,10 +3,10 @@ from mnist_extractor1 import MNISTExtractor
 
 # defining the parameters of the convolutional net
 kernel_size = 3  # needs to be uneven!!!
-depth = 3  # int(input("How many kernels should be used? > "))
+depth = int(input("How many kernels should be used? > "))
 fc_size = 1  # int(input("How many fully connected layers should be used? > "))
-learning_rate = 0.1  # float(input("Enter the learning rate! > "))
-epochs = 5  # int(round(float(input("How many epochs should be executed? > "))))
+learning_rate = float(input("Enter the learning rate! > "))
+epochs = int(round(float(input("How many epochs should be executed? > "))))
 batch_size = 5
 
 
@@ -16,28 +16,30 @@ cnn = ConvolutionalNet(kernel_size, depth, learning_rate, batch_size)
 
 # extracting the training labels and images
 train_labels = mne.extractLabels("mnist_data/train_labels.bin")
-custom_image_count = 20 #  cnn.enter_image_count(len(train_labels))
+custom_image_count = int(round(cnn.enter_image_count(len(train_labels)) / batch_size) * batch_size)
 train_images = mne.extractImages("mnist_data/train_images.bin", custom_image_count)
 
+# initializing the fully connected layer of the net
 image_size = len(train_images[0])
-cnn.initialitze_fully_conected(fc_size, image_size, custom_image_count)
+cnn.initialize_fully_connected(fc_size, image_size, custom_image_count)
+
+padded_image = cnn.generate_zero_padding(custom_image_count, train_images)  # adding a zero padding around the images
 
 for j in range(epochs):
     for i in range(0, custom_image_count, cnn.batch_size):
         # forward pass
         results = []
-        """train_images[i] = train_images[i].reshape(28, 28)  # reshaping the image into a 28 * 28 matrix
-        padded_image = cnn.generate_zero_padding(train_images[i])  # adding a zero padding around it
-        conv = [cnn.conv_layer(padded_image, x) for x in range(cnn.depth)]  # convolutional layer
+        conv = cnn.conv_layer(padded_image[i:i + cnn.batch_size])  # convolutional layer
         results.append(conv)  # depth lists of size image_size ^ 2
-        pooled = [cnn.pool(conv[x]) for x in range(cnn.depth)]  # pooling layer
+        pooled = cnn.pool(conv)  # pooling layer
         results.append(pooled)  # depth lists of size (image_size / 2) ^ 2
         # 1 list of size ((image_size / 2) * depth) ^ 2 with fc_size + 1 inner lists
-        results.append(cnn.fully_connected(pooled))"""
+        results.append(cnn.fully_connected(pooled))
+
         # backpropagation
-        # cnn.fc_backprop(train_labels[i], cnn.concentration(cnn.fully_connected(train_images[i])))
-        cnn.fc_backprop(train_labels[i], cnn.fully_connected(train_images[i:i + batch_size]))
+        fc_error = cnn.fc_backprop(train_labels[i:i + cnn.batch_size], results[-1])
         # pool_error = cnn.pooling_backprop(cnn.concentration(fc_error), results[len(results) - 3:len(results) - 1:])
         # cnn.convolutional_backprop(pool_error, results[0])
-        # print(cnn.convolutional_backprop(pool_error, results[0]))
-    print("accuracy: ", cnn.calculate_accuracy(train_labels, train_images, custom_image_count)[1])
+    train_error, accuracy = cnn.calculate_accuracy(train_labels, padded_image, custom_image_count)
+    print("Epoch {}: {} error: {} {} accuracy: {}%".format(j + 1, " " * abs(7 - len(str(j + 1))), train_error,
+                                                           " " * abs((7 - len(str(train_error)))), accuracy))
--- cnn_scratch.py
+++ cnn_scratch.py
@@ -4,14 +4,30 @@ import numpy as np
 class ConvolutionalNet:
 
     def __init__(self, kernel_size, depth, learning_rate, batch_size):
-        # kernels randomized according to normal distribution with mü=0 and sigma=sqrt(number of kernel parameters)
-        self.kernel = [np.random.normal(0.0, kernel_size, (kernel_size, kernel_size)) for x in range(depth)]
+        # kernels randomized according to normal distribution with mü=0 and sigma = sqrt(number of kernel parameters)
+        self.kernel = [np.random.randn(batch_size, kernel_size, kernel_size) * np.sqrt(kernel_size)
+                       for x in range(depth)]
         self.kernel_size = kernel_size
         self.depth = depth
         self.layers = []
         self.lRate = learning_rate
         self.pooling_stride = 2
         self.batch_size = batch_size
+
+    def relu(self, input, deriv=False):
+        """
+        activation function which is used after the convolutional layer. It returns a zero if the value is < 0 and
+        the same value if it is > 0
+        :param input: Float
+        :param deriv: Boolean
+        :return: Float
+        """
+        if not deriv:  # normal ReLU function
+            output = max([0, input])
+        elif deriv:  # derivative of the ReLU function
+            # returns a 1 if the input is greater than zero and a zero otherwise
+            output = float(np.greater(input, 0).astype(int))
+        return output
 
     def sig(self, x, deriv=False):
         """
@@ -26,80 +42,79 @@ class ConvolutionalNet:
         elif deriv:  # derivative of the sigmoid function (for an already squashed value)
             return x * (1 - x)
 
-    def softmax(self, x):
+    def softmax(self, x, deriv=False):
         """
-        softmax activision function, which brings all values in the array to a value between 0 and 1. Additionally the
+        softmax activation function, which brings all values in the array to a value between 0 and 1. Additionally the
         sum of the array values is 1
         :param x: np.array or list
-        :return: list of same size
+        :param deriv: Boolean
+        :return: list of same size as x
         """
-        normalized_array = np.exp(x) / sum(np.exp(x))
-        return list(normalized_array)
+        if not deriv:  # normal softmax function
+            return list(np.exp(x) / sum(np.exp(x)))
+        elif deriv:  # derivative of the softmax function (for an already squashed value)
+            return np.array(x) * (1 - np.array(x))
 
-    def generate_zero_padding(self, input_matrix):
+    def tanh(self, x, deriv=False):
+        """
+        tanh (hyperbolic tangent) activation function, which brings all values in the array to a value between -1 and 1.
+        tanh(x) = (e^x - e^-x) / (e^x + e^-x)
+        :param x: np.array or list
+        :param deriv: Boolean
+        :return: list of same size as x
+        """
+        if not deriv:  # normal tanh function
+            return np.tanh(x)
+        if deriv:  # derivative of the tanh function (for an already squashed value)
+            return 1 - np.square(x)
+
+    def generate_zero_padding(self, image_count, images):
         """
         generate a padding with the value 0.0 around the outside of the image matrix to keep its size constant after the
         convolutional layer
         :param input_matrix: np.matrix containing 28 * 28 Mnist image
         :return: np. matrix of original image with layer of zeros around the edges
         """
+        outputs = []
         padding_size = int((self.kernel_size - 1) / 2)
-        output_matrix = np.zeros((len(input_matrix) + 2 * padding_size, len(input_matrix) + 2 * padding_size))
-        output_matrix[padding_size: -padding_size, padding_size: -padding_size] = input_matrix
-        return output_matrix
+        image_size = int(np.sqrt(len(images[0])))
+        output_matrix = np.zeros((image_size + 2 * padding_size, image_size + 2 * padding_size))
+        # create a new matrix of the output size and past the original image in its middle
+        for i in range(image_count):
+            image = images[i].reshape(image_size, image_size)
+            output_matrix[padding_size: -padding_size, padding_size: -padding_size] = image
+            outputs.append(output_matrix)
+        return outputs
 
-    def relu(self, input, deriv=False):
-        """
-        activition function which is used after the convolutional layer. It returns a zero if the value is < 0 and
-        the same value if it is > 0
-        :param input: np.matrix
-        :return: np.matrix of same size
-        """
-        """output = input
-        for i in range(len(input)):
-            for j in range(len(input)):
-                if not deriv:
-                    output[i, j] = max([0, input[i, j]])
-                else:
-                    output[i, j] = np.greater(input[i, j], 0).astype(int)"""
-        if not deriv:
-            if type(input) is int:
-                output = max([0, input])
-            else:
-                output = [max(0, x) for x in input]
-        else:
-            output = np.greater(input, 0).astype(int)  # returns a 1 if the input is greater than zero and a zero otherwise
-        return output
-
-    def conv_layer(self, input_matrix, iteration):
+    def conv_layer(self, input_matrix):
         """
         convolutional layer of the network (stride = 1)
-        :param input_matrix: np.matrix with zero padding
-        :param iteration: integer: number of the kernel
-        :return: np.matrix of size 28 * 28
-        """
-        input_size = len(input_matrix)
-        # convolutional layer
-        output_size = input_size - self.kernel_size + 1
-        output = np.zeros((output_size, output_size))
-        # k, h for scanning over the input matrix
-        for k in range(output_size):
-            for h in range(output_size):
-                # j, i for Frobenius inner product (here: sum)
-                sum = 0
-                for j in range(self.kernel_size - 1):
-                    for i in range(self.kernel_size - 1):
-                            sum += (input_matrix[k + j, h + i] * self.kernel[iteration][j, i])
-                output[k, h] = self.relu(sum, False)
-        return output
-
-    def pool(self, input_matrix):
-        """
-        max pooling: only filtering out the maximum value of a stride * stride square of the input matrix
-        :param input_matrix: np.matrix of size 28 * 28
-        :return: np.matrix of size 14 * 14
+        :param input_matrix: list containing {batch_size} np.matrices with zero padding
+        :return: {batch_size} lists each containing {depth} np.matrices of size 28 * 28
         """
         input_size = len(input_matrix[0])
+        output_size = input_size - self.kernel_size + 1
+        output = [[np.zeros((output_size, output_size)) for i in range(self.depth)] for j in range(self.batch_size)]
+        for c in range(self.batch_size):
+            for x in range(self.depth):
+                # k, h for scanning over the input matrix
+                for k in range(output_size):
+                    for h in range(output_size):
+                        # j, i as iterators for Frobenius inner product (here: inner_sum)
+                        inner_sum = 0
+                        for j in range(self.kernel_size - 1):
+                            for i in range(self.kernel_size - 1):
+                                    inner_sum += (input_matrix[c][k + j, h + i] * self.kernel[x][c, j, i])
+                        output[c][x][k, h] = self.relu(inner_sum, False)
+        return output
+
+    def pool(self, inputs):
+        """
+        max pooling: only filtering out the maximum value of a stride * stride square of the input matrix
+        :param inputs: {batch_size} lists each containing {depth} np.matriced of size 28 * 28
+        :return: {batch_size} lists each containing {depth} np.matrices of size 14 * 14
+        """
+        input_size = len(inputs[0][0])
         # testing if the size of the matrix is dividable by 2/ 3 to determine the stride
         if input_size % 2 == 0:
             self.pooling_stride = 2
@@ -109,32 +124,36 @@ class ConvolutionalNet:
             print("matrix of uneven length: no pooling possible")
 
         output_size = int(input_size / self.pooling_stride)
-        output = np.zeros([output_size, output_size])
-        for i in range(0, input_size, self.pooling_stride):
-            for j in range(0, input_size, self.pooling_stride):
-                # scaling the original matrix to squares of size stride^2 and adding their max value to an output matrix
-                pooling_field = input_matrix[i:(i + self.pooling_stride), j:(j + self.pooling_stride)]
-                output[int(i / self.pooling_stride), int(j / self.pooling_stride)] = pooling_field.max()
-                # output[x, y] = np.max(output[x, y], input_matrix[x + i, y + j])
+        output = [[np.zeros([output_size, output_size]) for i in range(self.depth)] for j in range(self.batch_size)]
+
+        for d in range(self.depth):
+            for b in range(self.batch_size):
+                for g in range(0, input_size, self.pooling_stride):
+                    for h in range(0, input_size, self.pooling_stride):
+                        # i, j as iterators for scanning over the pooling field
+                        for i in range(self.pooling_stride):
+                            for j in range(self.pooling_stride):
+                                output[b][d][int(g / 2), int(h / 2)] = max(output[b][d][int(g / 2), int(h / 2)],
+                                                                           inputs[b][d][g + i, h + j])
         return output
 
-    def concentraation(self, input, backward=False):
+    def concentration(self, input, backward=False):
         """
         reformation of the list of matrices to a vector before the fully-connected layer/ otherwise in backpropagation
         :param input: list of matrices/ np.array
-        :param backward:
+        :param backward: Boolean
         :return:
         """
         output = []
         if not backward:
-            for i in range(self.depth):
-                output.extend(input[i].flatten())
+            for j in range(self.depth):
+                output.extend(input[j].flatten())
             output = np.array(output)
         elif backward:
             output = np.array_split(input, self.depth)  # returns list of np.arrays
         return output
 
-    def initialitze_fully_conected(self, fc_size, input_image_size, image_count):
+    def initialize_fully_connected(self, fc_size, input_image_size, image_count):
         """
         initializing the layers of the fully connected layer. The size of the first fc layer is equal to the size of
         the MNIST image divided by 2 (because of the pooling), the hidden layers all have size 100 * 100 and the output
@@ -147,35 +166,34 @@ class ConvolutionalNet:
         np.random.seed(0)
         layer_size = self.depth * int(np.sqrt(input_image_size) / 2) ** 2
         # generating a transition layer, a user defined number of hidden layers of size 100 * 100 and an output layer
-        self.layers = [np.random.randn(self.batch_size, 784, 100) * (1 / np.sqrt(image_count))]
+        self.layers = [np.random.randn(self.batch_size, layer_size, 100) * (1 / np.sqrt(image_count))]
         for j in range(fc_size):
             self.layers.append(np.random.randn(self.batch_size, 100, 100))
         self.layers.append(np.random.randn(self.batch_size, 100, 10))
         return
 
-    def fully_connected(self, input_matrix):
+    def fully_connected(self, input_matrix, concentrate=True):
         """
-        flattens and adds together the matrixes resulting from the last pooling step to one vector and then multiplies
-        the vector step by step with all the weigth matrices
-        :param input_matrix: list of np.matrices
-        :return:
+        multiplies the vector step by step with all the weigth matrices. All the results are stored in the outputs
+        vector. The first value has size 588, the following have size 100 and the last value has size 10.
+        :param input_matrix: {batch_size} lists each containing {depth} np.matrices of size 14 * 14
+        :param concentrate: Boolean
+        :return: {batch_size} lists each containing {fc_size} np.arrays
         """
-        # step left out because no convolutional layer is applied
-        # image = concentration(input_matrix)
-        """image = []
-        for i in range(self.depth):
-            image.extend(input_matrix[i].flatten())
-        image = np.array(image)"""
         outputs = [[] for x in range(self.batch_size)]
-        # multiplying the input vector with all the hidden layer matrices
+
         for i in range(self.batch_size):
-            image = np.array(input_matrix[i])
+            if concentrate:
+                image = np.array(self.concentration(input_matrix[i]))
+            else:
+                image = input_matrix[i]
             outputs[i].append(image)
-            for j in range(len(self.layers)):
-                outputs[i].append(self.relu(np.dot(np.array(image).T, self.layers[j][i])))
+            # multiplying the input vector with all the hidden layer matrices
+            for j in range(len(self.layers) - 1):
+                outputs[i].append(self.tanh(np.dot(np.array(image).T, self.layers[j][i])))
                 image = outputs[i][j + 1]
-            outputs[i].append(self.softmax(outputs[i][j + 1]))
-        print(outputs)
+            # softmax activation function for the last layer
+            outputs[i].append(self.softmax(np.dot(np.array(image).T, self.layers[j + 1][i])))
         return outputs
 
     def fc_backprop(self, labels, results):
@@ -185,21 +203,26 @@ class ConvolutionalNet:
         :param results: List of np arrays
         :return:
         """
-        target = [0.0] * 10
-        target[labels] = 1.0  # list of targets (for all nodes zero, but for the actual label of the image)
         hidden_errors = [[] for x in range(self.batch_size)]
         for i in range(self.batch_size):
-            error = target - results[i][len(self.layers)]  # error of the last layer
+            target = [0.0] * 10
+            target[labels[i]] = 1.0  # list of targets (for all nodes zero, but for the actual label of the image)
+            error = target - np.array(results[i][len(self.layers)])  # error of the last layer
             hidden_errors[i].append(error)  # list of all the hidden errors (error of last layer 1st in list!)
             for j in range(len(self.layers)):
                 # calculating the hidden errors by multiplying the error of the 1st layer with the hidden layer matrices
                 hidden_errors[i].append(np.dot(self.layers[len(self.layers) - (j + 1)][i], error))
                 error = hidden_errors[i][j + 1]
                 # adjust weights
-                correction = self.lRate * np.outer((hidden_errors[i][j] * self.sig(results[i][len(self.layers) - j], True)),
+                if j == 0:
+                    # again using the derivative of the softmax function for the results of the last layer
+                    hidden_out = self.softmax(results[i][len(self.layers) - j], True)
+                else:
+                    hidden_out = self.tanh(results[i][len(self.layers) - j], True)
+                correction = self.lRate * np.outer((hidden_errors[i][j] * hidden_out),
                                                    results[i][len(self.layers) - (j + 1)]).T
-                self.layers[len(self.layers) - (j + 1)] += correction
-        return
+                self.layers[len(self.layers) - (j + 1)][i] += correction
+        return hidden_errors
 
     def pooling_backprop(self, fc_error, results):
         """
@@ -211,16 +234,17 @@ class ConvolutionalNet:
         # recreating the matrices from the
         # fc_error = np.array_split(fc_error, self.depth)
         input_size = len(results[len(results) - 1][0])
-        output_size = 2 * input_size
-        pooling_error = [np.zeros((output_size, output_size)) for x in range(self.depth)]
-        for h in range(self.depth):
-            for i in range(0, output_size, self.pooling_stride):
-                for j in range(0, output_size, self.pooling_stride):
-                    for p in range(self.pooling_stride):
-                        for q in range(self.pooling_stride):
-                            if results[len(results) - 1][h][int(i / 2), int(j / 2)] == \
-                                    results[len(results) - 2][h][i + p, j + q]:
-                                pooling_error[h][i + p, j + q] = fc_error[h][i + j]
+        out_size = 2 * input_size
+        pooling_error = [[np.zeros((out_size, out_size)) for x in range(self.depth)] for y in range(self.batch_size)]
+        for b in range(self.batch_size):
+            for h in range(self.depth):
+                for i in range(0, out_size, self.pooling_stride):
+                    for j in range(0, out_size, self.pooling_stride):
+                        for p in range(self.pooling_stride):
+                            for q in range(self.pooling_stride):
+                                if results[len(results) - 1][h][int(i / 2), int(j / 2)] == \
+                                        results[len(results) - 2][h][i + p, j + q]:
+                                    pooling_error[b][h][i + p, j + q] = fc_error[h][i + j]
         return pooling_error
 
     def convolutional_backprop(self, pooling_error, results):
@@ -278,13 +302,20 @@ class ConvolutionalNet:
         :return accuracy: Float
         """
         error = 0
-        for i in range(image_count):
-            target = labels[i]
-            prediction_list = list(self.fully_connected(images[i])[-1])  # activations of the output layer
-            prediction = prediction_list.index(max(prediction_list))  # node with the highest activation
-            # print(target, prediction_list, prediction)
-            if target != prediction:
-                error += 1
+        for i in range(0, image_count, self.batch_size):
+            conv = self.conv_layer(images[i:i + self.batch_size])  # convolutional layer
+            # results.append(conv)  # depth lists of size image_size ^ 2
+            pooled = self.pool(conv)  # pooling layer
+            # results.append(pooled)  # depth lists of size (image_size / 2) ^ 2
+            # 1 list of size ((image_size / 2) * depth) ^ 2 with fc_size + 1 inner lists
+            results = self.fully_connected(pooled)
+            for j in range(self.batch_size):
+                target = labels[i + j]
+                prediction_list = results[j][-1]  # activations of the output layer
+                prediction = prediction_list.index(max(prediction_list))  # node with the highest activation
+                # print(target, prediction_list, prediction)
+                if target != prediction:
+                    error += 1
         accuracy = 100 - (error / image_count * 100)  # calculate part of right guesses (in percent)
         # self.accuracy_list.append(accuracy)
         return error, accuracy
